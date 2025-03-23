f2bd7a4f18de37ab9d227a3e411c253f5dcec17d
Hampus Farner <hampusfarner@outlook.com>
2025-03-23 08:18:53 +0100

    Update populatedb script

--- db/populatedb.js
+++ db/populatedb.js
@@ -1,7 +1,69 @@
 #! /usr/bin/env node
-
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({
+  path: path.resolve(__dirname, '../.env.production'),
+});
+const { Client } = require('pg');
 
 // connect to db
 // check if tables exists
 // or more specifically create table if does not exist with sql
+
+// const {Client} = require('pg');
+const SQL = `
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    email text NOT NULL,
+    firstname text NOT NULL,
+    lastname text NOT NULL,
+    passwordhash text NOT NULL,
+    salt text NOT NULL,
+    is_admin boolean DEFAULT false NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS forum (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    title TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    user_id INTEGER,
+    forum_id INTEGER,
+    title TEXT NOT NULL,
+    context TEXT NOT NULL,
+    date_time timestamp without time zone NOT NULL,
+    CONSTRAINT fk_user_id
+      FOREIGN KEY(user_id)
+        REFERENCES users(id),
+    CONSTRAINT fk_forum_id
+      FOREIGN KEY(forum_id)
+        REFERENCES forum(id)
+);
+
+INSERT INTO forum (title) SELECT 'General'
+  WHERE NOT EXISTS (SELECT * FROM forum);`;
+
+async function main() {
+  console.log('seeding');
+  console.log(process.env.DB_HOST);
+  const client = new Client({
+    connectionString: `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+  });
+  try {
+    await client.connect();
+    console.log('connected');
+    await client.query(SQL);
+    console.log('db created successfully');
+  } catch (error) {
+    console.error('error setting up', error);
+  } finally {
+    await client.end();
+    console.log('db connection closed');
+  }
+  console.log('done');
+}
+
+// now getting error with one of the syntax (WHERE). progress!
+
+main();
